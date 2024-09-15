8b00ed0e6ca372eeb28f22c39f0523fea6e32485
Mariia Butolina <butolinkam@gmail.com>
2024-09-15 15:35:20 -0400

    week 14

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema({
+    company:{
+        type: String,
+        required: [true, "Please provide company's name"],
+        maxlength:50
+    },
+    position:{
+        type: String,
+        required: [true, "Please provide position"],
+        maxlength:150
+    },
+    status:{
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default:'pending'
+    },
+    createdBy:{
+        type: mongoose.Types.ObjectId,
+        ref:'User',
+        required: [true, "Please provide User"]
+    }
+}, {timestamps:true});
+
+module.exports= mongoose.model('Job', JobSchema);
